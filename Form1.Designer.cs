cbbb7a51bb89ad4d7be688b4e63f3fc28fd01491
Ömer Akkoyun <33864154+OmerAkkoyun@users.noreply.github.com>
2018-10-03 20:32:06 +0300

    C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace _4.uygulama_Kronometre
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dakikaLabel = new System.Windows.Forms.Label();
+            this.saniyeLabel = new System.Windows.Forms.Label();
+            this.saliseLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.LimeGreen;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BAŞLAT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightGray;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(12, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "DURDUR";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(174, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dakika";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MediumBlue;
+            this.label2.Location = new System.Drawing.Point(304, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Saniye";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(432, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Salise";
+            // 
+            // dakikaLabel
+            // 
+            this.dakikaLabel.AutoSize = true;
+            this.dakikaLabel.Font = new System.Drawing.Font("Arial Black", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dakikaLabel.ForeColor = System.Drawing.Color.DarkBlue;
+            this.dakikaLabel.Location = new System.Drawing.Point(198, 82);
+            this.dakikaLabel.Name = "dakikaLabel";
+            this.dakikaLabel.Size = new System.Drawing.Size(51, 56);
+            this.dakikaLabel.TabIndex = 5;
+            this.dakikaLabel.Text = "0";
+            // 
+            // saniyeLabel
+            // 
+            this.saniyeLabel.AutoSize = true;
+            this.saniyeLabel.Font = new System.Drawing.Font("Arial Black", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.saniyeLabel.ForeColor = System.Drawing.Color.MediumBlue;
+            this.saniyeLabel.Location = new System.Drawing.Point(322, 82);
+            this.saniyeLabel.Name = "saniyeLabel";
+            this.saniyeLabel.Size = new System.Drawing.Size(51, 56);
+            this.saniyeLabel.TabIndex = 6;
+            this.saniyeLabel.Text = "0";
+            // 
+            // saliseLabel
+            // 
+            this.saliseLabel.AutoSize = true;
+            this.saliseLabel.Font = new System.Drawing.Font("Arial Black", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.saliseLabel.ForeColor = System.Drawing.Color.Blue;
+            this.saliseLabel.Location = new System.Drawing.Point(451, 82);
+            this.saliseLabel.Name = "saliseLabel";
+            this.saliseLabel.Size = new System.Drawing.Size(51, 56);
+            this.saliseLabel.TabIndex = 7;
+            this.saliseLabel.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightGray;
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.DarkOrange;
+            this.button3.Location = new System.Drawing.Point(12, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 60);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "DURAKLAT";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(533, 216);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.saliseLabel);
+            this.Controls.Add(this.saniyeLabel);
+            this.Controls.Add(this.dakikaLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Kronometre";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label dakikaLabel;
+        private System.Windows.Forms.Label saniyeLabel;
+        private System.Windows.Forms.Label saliseLabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4.uygulama_Kronometre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int dakika = 0, saniye = 0, salise = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 60000;
+            timer2.Interval = 1000;
+            timer3.Interval = 10;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (saniye==59)
+            {
+                saniye = -1;//aradaki 1salise farkını kapatmak için..
+            } 
+            saniye += 1;
+            saniyeLabel.Text = Convert.ToString(saniye);
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (salise==99)
+            {
+                salise = 0;
+            }
+            salise += 1;
+            saliseLabel.Text = Convert.ToString(salise);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dakika = 0; saniye = 0; salise = 0;
+            dakikaLabel.Text = Convert.ToString(dakika);
+            saniyeLabel.Text = Convert.ToString(saniye);
+            saliseLabel.Text = Convert.ToString(salise);
+
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer2.Start();
+            timer3.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            dakika += 1;
+            dakikaLabel.Text = Convert.ToString(dakika);
+        }
+    }
+}
